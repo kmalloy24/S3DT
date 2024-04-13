253dcdb42ed51363cb8b31476ba99b2aa3e7c47d
Kyle Malloy <kmalloy24@gmail.com>
2024-04-13 18:39:46 -0400

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Supa Simple Svelte DataTable (s3dt)
+
+## Easily turn your Supabase tables into powerful front-end components
+
+``` html
+<DataTable {data}>
+```
+
+Built with Svelte 4 and [Svelte Simple Datatables](https://vincjo.fr/datatables/remote/home).
+
+> [!NOTE]  
+> This is an opinionated template using Sveltekit 2 and Skeleton UI.
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { s3dtTheme } from './src/s3dtTheme'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		skeleton({
+			themes: {
+				custom: [
+					s3dtTheme,
+				],
+			},
+		}),
+	],
+} satisfies Config;
